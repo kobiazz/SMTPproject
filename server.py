87a799cbf5610a0598bac4b86b1f060772170bd5
tamiryak <45327624+tamiryak@users.noreply.github.com>
2020-06-14 14:23:10 +0300

    Update server.py

--- server.py
+++ server.py
@@ -13,11 +13,12 @@ from tkinter import filedialog, font
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler, PatternMatchingEventHandler
 
-# main for listener
+# if statment for not open gui on aws server so it can works on aws
 if os.environ.get('DISPLAY', '') == '':
     print('no display found. Using :0.0')
     os.environ.__setitem__('DISPLAY', ':0.0')
 
+ #main for intialize folder listener variables
 if __name__ == "__main__":
     patterns = "*"
     ignore_patterns = ""
@@ -25,32 +26,35 @@ if __name__ == "__main__":
     case_sensitive = True
     my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
 
-app = Flask(__name__)
+app = Flask(__name__) #start working with flask 
 path =""
 email=""
-thisdict = {"path": path , "email":email}
+thisdict = {"path": path , "email":email} #we used dictionary so we can save the email and path even when we close the web
 
+#the flask using the route of the site - this is the first page
 @app.route("/")
 def index():
-    return render_template('index.html',path=thisdict["path"],email=thisdict["email"])
+    return render_template('index.html',path=thisdict["path"],email=thisdict["email"]) #sending the variables into the page so we can see it
 
-
+#if user pressed browse we initate program to open folder picker
 @app.route("/browse", methods=['POST'])
 def browse():
-    command = request.form['x']
-    thisdict['path'] = filedialog.askdirectory()
-    thisdict['email'] = request.form['email']
-    return index()
+    command = request.form['pathWeb'] # get the path from web for user to see
+    thisdict['path'] = filedialog.askdirectory() #get the path and save it in the dictionary
+    thisdict['email'] = request.form['email'] #get the email and save it in the dictionary
+    return index() #continue working
 
+#user pressed execute and the listener start to listen to the path
 @app.route("/execute", methods=['POST'])
 def execute():
     search()
     return index()
 
 
-def search():  # command of search button
-    path = thisdict['path']
-    thisdict["email"] = thisdict['email']
+#this is the function of the observer
+def search():  
+    path = thisdict['path'] 
+    thisdict["email"] = thisdict['email'] 
     go_recursively = True
     my_observer = Observer()
     my_observer.schedule(my_event_handler, path, recursive=go_recursively)
@@ -65,6 +69,7 @@ def search():  # command of search button
 
     return ''
 
+#function of the message sending - work when listener get file
 def Sendm(s, x):
     subject = "new file"
     body = "This is an email with attachment sent from Python"
@@ -82,11 +87,11 @@ def Sendm(s, x):
     # Add body to email
     message.attach(MIMEText(body, "plain"))
 
+     # filename = "123a.jpg"  # In same directory as script
     filename = s.replace('\\', '/')
-    # filename = "123a.jpg"  # In same directory as script
+
 
     # Open  file in binary mode
-
     with open(filename, "rb+") as attachment:
         # Add file as application/octet-stream
         # Email client can usually download this automatically as attachment
@@ -113,17 +118,15 @@ def Sendm(s, x):
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, text)
 
-
+#when file is created - the observer will notify and the function will start to work
 def on_created(event):
-    email = thisdict.get("email")
+    email = thisdict.get("email") #getting the email and set it to the fun
     Sendm(str(event.src_path), email)
-    print("the mail was sent")
+    print("the mail was sent") 
     
 
 
-my_event_handler.on_created = on_created
-
-
+my_event_handler.on_created = on_created 
 
 
 if __name__ == "__main__":
